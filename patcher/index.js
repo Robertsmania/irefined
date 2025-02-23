b515cc25a4e2110d108a71ae3838159fdbe04624
Jason Murray <jsn256@gmail.com>
2025-02-23 22:53:12 +0000

    4x

--- patcher/index.js
+++ patcher/index.js
@@ -1,11 +1,34 @@
+const process = require('process')
 const patchAsar = require('patch-asar')
 const path = require('path')
+const fs = require('fs')
 
-const inputAsar = path.join(__dirname, "app.asar")
+const pf86 = process.env['ProgramFiles(x86)']
+const irPath = path.join(pf86, "/iRacing/ui/resources/")
+const inputAsar = path.join(irPath, "app.asar")
 const patchFolder = path.join(__dirname, "patches")
 
-patchAsar(inputAsar, patchFolder).then(()=>{
-	console.log("Successfully Patched .asar archive in place")
-}).catch(error => {
-	console.log(error)
-})
+if (!fs.existsSync(irPath + '.irefined')) {
+
+	fs.openSync(irPath + '.irefined', 'w');
+
+	if (!fs.existsSync(inputAsar + '.bak')) {
+		console.log("Backing up app.asar")
+		fs.copyFile(inputAsar, inputAsar + '.bak', (err) => {
+			if (err) throw err;
+		});
+	}
+
+	console.log("Installing iRefined...")
+
+	fs.cpSync(path.join(__dirname, "iref"), path.join(irPath, "iref"), {recursive: true});
+
+	patchAsar(inputAsar, patchFolder).then(() => {
+		console.log("Success!")
+	}).catch(error => {
+		console.log(error)
+	})
+
+} else {
+	console.log("iRefined already installed!")
+}
--- patcher/iref/bootstrap.js
+++ patcher/iref/bootstrap.js
@@ -6,11 +6,11 @@ let irefBootstrap = setInterval(() => {
         let link = document.createElement("link");
         link.type = "text/css";
         link.rel = "stylesheet";
-        link.href = "http://127.0.0.1:8080/extension.css?" + new Date().getTime();
+        link.href = "https://jason-murray.github.io/irefined/extension.css?" + new Date().getTime();
         head.appendChild(link);
     
         let script = document.createElement('script');
-        script.src = "http://127.0.0.1:8080/main.js?" + new Date().getTime();
+        script.src = "https://jason-murray.github.io/irefined/main.js?" + new Date().getTime();
         script.type = 'module';
         head.appendChild(script);
     }
