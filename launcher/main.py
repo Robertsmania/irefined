27e41c6c80c133d635a0ff0530d434ac1cfd25e0
Jason Murray <jsn256@gmail.com>
2025-07-03 13:54:56 +0100

    Improve error handling for WebSocket connection and streamline update process

--- launcher/main.py
+++ launcher/main.py
@@ -47,7 +47,8 @@ def get_websocket_url():
             return tabs[0]['webSocketDebuggerUrl']
         except Exception:
             time.sleep(1)
-    raise RuntimeError("Could not connect to Electron's remote debugger")
+    messagebox.showerror("iRefined", "Failed to connect to iRacing UI.\nIs iRacing down for maintenance?")
+    sys.exit(1)
 
 async def run_js_from_file(ws_url, js_path):
     with open(js_path, 'r') as f:
@@ -61,32 +62,32 @@ async def run_js_from_file(ws_url, js_path):
         }))
 
 def check_update():
+    manager = velopack.UpdateManager("https://github.com/jason-murray/irefined/releases/latest/download")
+    
     update_info = manager.check_for_updates()
     if not update_info:
         return # no updates available
 
     answer = messagebox.askyesno("iRefined Update", "Update? (Strongly recommended)")
     if answer:
-        do_update()
+            # Download the updates
+            manager.download_updates(update_info)
+
+            # Apply the update and restart the app
+            manager.apply_updates_and_restart(update_info)
     else:
         root.destroy()
 
-def do_update(update_info):
-    # Download the updates
-    manager.download_updates(update_info)
-
-    # Apply the update and restart the app
-    manager.apply_updates_and_restart(update_info)
-
 def main():
-    check_update()
+    if getattr(sys, "frozen", False):
+        check_update()
+
     launch_electron()
     ws_url = get_websocket_url()
     asyncio.run(run_js_from_file(ws_url, find_data_file(JS_FILE_PATH)))
 
 root = tk.Tk()
 root.withdraw()
-manager = velopack.UpdateManager("https://github.com/jason-murray/irefined/releases/latest/download")
 
 if __name__ == "__main__":
     velopack.App().run()
