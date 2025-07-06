ec98a3276b0f177e5de23c3643aa329a88aef6d8
Jason Murray <jsn256@gmail.com>
2025-07-06 02:14:43 +0100

    Changes to how CDP port is configured and extension is bootstrapped in to the UI.

--- launcher/main.py
+++ launcher/main.py
@@ -1,14 +1,14 @@
 import asyncio
+import configparser
 import json
 import os
-from pathlib import Path
-import subprocess
 import sys
 import threading
+from tkinter import filedialog, messagebox
+import tkinter
 import webbrowser
-from os import path
+from os import makedirs, path
 
-import psutil
 import pystray
 import requests
 import velopack
@@ -18,7 +18,15 @@ from PIL import Image
 
 DISCORD_URL = "https://discord.gg/hxVf8wcGaV"
 JS_FILE_PATH = "bootstrap.js"
-REMOTE_DEBUGGING_PORT = 9222
+SHORTCUT_PATH = os.path.join(
+    os.environ["APPDATA"],
+    "Microsoft\\Windows\\Start Menu\\Programs\\Startup",
+    "iRefined.lnk",
+)
+CONFIG_INI_DIR = path.join(path.expanduser("~"), "Documents", "iRefined")
+CONFIG_INI_FILE = path.join(CONFIG_INI_DIR, "irefined.ini")
+
+seen_socket_urls = set()
 
 
 def find_data_file(filename):
@@ -31,10 +39,21 @@ def find_data_file(filename):
     return path.join(datadir, filename)
 
 
-STARTUP_FOLDER = os.path.join(
-    os.environ["APPDATA"], "Microsoft\\Windows\\Start Menu\\Programs\\Startup"
-)
-SHORTCUT_PATH = os.path.join(STARTUP_FOLDER, "iRefined.lnk")
+def create_default_config():
+    # Create default config file in Documents/iRefined/
+    if not path.exists(CONFIG_INI_DIR):
+        makedirs(CONFIG_INI_DIR)
+
+    config = configparser.ConfigParser()
+    config["Config"] = {
+        "IRACING_PATH": set_iracing_install(),
+        "PORT": "9222",
+    }
+
+    with open(CONFIG_INI_FILE, "w") as configfile:
+        config.write(configfile)
+
+    return config
 
 
 def create_startup_shortcut():
@@ -47,21 +66,60 @@ def create_startup_shortcut():
     print("[INFO] Startup enabled.")
 
 
-def remove_startup_shortcut():
-    if os.path.exists(SHORTCUT_PATH):
-        os.remove(SHORTCUT_PATH)
-        print("[INFO] Startup disabled.")
+def load_config():
+    config = configparser.ConfigParser()
+
+    if path.exists(CONFIG_INI_FILE):
+        config.read(CONFIG_INI_FILE)
+    else:
+        config = create_default_config()
+        create_startup_shortcut()
+
+    return config
+
+
+def save_config(config):
+    with open(CONFIG_INI_FILE, "w") as configfile:
+        config.write(configfile)
+
+
+def set_iracing_install():
+    if path.exists("C:\\Program Files (x86)\\iRacing\\"):
+        return "C:\\Program Files (x86)\\iRacing\\"
+
+    messagebox.showinfo(
+        "iRefined",
+        "Please locate your iRacing install folder.\n\nUsually found at:\nC:\\Program Files (x86)\\iRacing\\\n\nNOT the one in Documents!",
+    )
+
+    iracing_install_path = filedialog.askdirectory(
+        title="Select iRacing Install Folder",
+        initialdir="C:\\",
+    )
+
+    return iracing_install_path
+
+
+config = load_config()
+IRACING_PATH = config.get("Config", "IRACING_PATH")
+REMOTE_DEBUGGING_PORT = config.getint("Config", "PORT")
 
 
 def is_startup_enabled():
     return os.path.exists(SHORTCUT_PATH)
 
 
+def remove_startup_shortcut():
+    if is_startup_enabled():
+        os.remove(SHORTCUT_PATH)
+        print("[INFO] Startup disabled.")
+
+
 def get_websocket_url():
     try:
-        tabs = requests.get(f"http://localhost:{REMOTE_DEBUGGING_PORT}/json").json()
-        # loop tabs to find one with title "iRacing"
-        tabs = [tab for tab in tabs if tab.get("title") == "iRacing"]
+        tabs = requests.get(f"http://127.0.0.1:{REMOTE_DEBUGGING_PORT}/json").json()
+
+        tabs = [tab for tab in tabs if ".iracing.com/web/racing" in tab.get("url")]
         if not tabs:
             return
         return tabs[0]["webSocketDebuggerUrl"]
@@ -73,82 +131,30 @@ async def run_js_from_file(ws_url, js_path):
     with open(js_path, "r") as f:
         js_code = f.read()
 
-    async with websockets.connect(ws_url) as ws:
-        await ws.send(
-            json.dumps(
-                {
-                    "id": 1,
-                    "method": "Runtime.evaluate",
-                    "params": {"expression": js_code},
-                }
-            )
-        )
-
-
-def check_update():
-    if getattr(sys, "frozen", False):
-        manager = velopack.UpdateManager(
-            "https://github.com/jason-murray/irefined/releases/latest/download"
-        )
-
-        update_info = manager.check_for_updates()
-        if not update_info:
-            print("[INFO] No updates available.")
-            return
-
-        # Download and apply updates
-        print("[INFO] Update available! Applying...")
-        manager.download_updates(update_info)
-        manager.apply_updates_and_restart(update_info)
-
-
-def find_instances_without_iref():
-    processes = []
-    for proc in psutil.process_iter(attrs=["pid", "name", "cmdline"]):
-        try:
-            if proc.info["name"] == "iRacingUI.exe":
-                cmdline = proc.info.get("cmdline", [])
-                if (
-                    "--remote-debugging-port={REMOTE_DEBUGGING_PORT}" not in cmdline
-                    and len(cmdline) < 2
-                ):
-                    processes.append(proc)
-        except (psutil.AccessDenied, psutil.NoSuchProcess):
-            continue
-    return processes
-
-
-def restart_with_iref(proc):
-    iracing_ui_path = proc.cmdline()[0] if proc.cmdline() else None
-
     try:
-        print(f"[INFO] Terminating PID {proc.pid}...")
-        proc.terminate()
-        proc.wait(timeout=5)
-    except psutil.TimeoutExpired:
-        print(f"[WARN] PID {proc.pid} did not terminate in time. Forcing.")
-        proc.kill()
-    except (psutil.NoSuchProcess, psutil.AccessDenied) as e:
-        print(f"[ERROR] Could not terminate process: {e}")
-        return
-
-    print("[INFO] Restarting with debug port flag...")
-    subprocess.Popen(
-        [iracing_ui_path, f"--remote-debugging-port={REMOTE_DEBUGGING_PORT}"],
-        stdout=subprocess.DEVNULL,
-        start_new_session=True,
-    )
-
-
-def monitor_process():
-    while True:
-        ir_procs = find_instances_without_iref()
-        for proc in ir_procs:
-            restart_with_iref(proc)
+        async with websockets.connect(ws_url) as ws:
+            await ws.send(
+                json.dumps(
+                    {
+                        "id": 1,
+                        "method": "Page.addScriptToEvaluateOnNewDocument",
+                        "params": {"source": js_code, "runImmediately": True},
+                    }
+                )
+            )
+            seen_socket_urls.add(ws_url)
+            print(f"[INFO] Successfully injected script into {ws_url}")
+    except (
+        websockets.exceptions.ConnectionClosed,
+        websockets.exceptions.WebSocketException,
+        OSError,
+    ) as e:
+        print(f"[ERROR] Failed to send script to WebSocket: {e}")
+    except Exception as e:
+        print(f"[ERROR] Unexpected error with WebSocket: {e}")
 
 
 def monitor_websocket():
-    seen_socket_urls = set()
 
     while True:
         ws_url = get_websocket_url()
@@ -156,7 +162,6 @@ def monitor_websocket():
             continue
         if ws_url not in seen_socket_urls:
             print(f"[INFO] Found new iRacing WebSocket URL: {ws_url}")
-            seen_socket_urls.add(ws_url)
             asyncio.run(run_js_from_file(ws_url, find_data_file(JS_FILE_PATH)))
 
 
@@ -179,18 +184,52 @@ def toggle_startup(icon, item):
     icon.update_menu()
 
 
+def check_update():
+    if getattr(sys, "frozen", False):
+        manager = velopack.UpdateManager(
+            "https://github.com/jason-murray/irefined/releases/latest/download"
+        )
+
+        update_info = manager.check_for_updates()
+        if not update_info:
+            print("[INFO] No updates available.")
+            return
+
+        # Download and apply updates
+        print("[INFO] Update available! Applying...")
+        manager.download_updates(update_info)
+        manager.apply_updates_and_restart(update_info)
+
+
+def create_config_json():
+    local_json_dir = path.join(IRACING_PATH, "ui", "config")
+    if not path.exists(local_json_dir):
+        makedirs(local_json_dir)
+
+    local_json_path = path.join(local_json_dir, "local.json")
+
+    config_data = {
+        "scorpioDebugPort": REMOTE_DEBUGGING_PORT,
+    }
+    with open(local_json_path, "w") as f:
+        json.dump(config_data, f, indent=4)
+    print("[INFO] Created local.json")
+
+
 def main():
     check_update()
-
-    threading.Thread(target=monitor_process, daemon=True).start()
+    create_config_json()
     threading.Thread(target=monitor_websocket, daemon=True).start()
-
     icon.run()
 
 
 if __name__ == "__main__":
     if getattr(sys, "frozen", False):
         velopack.App().run()
+
+    root = tkinter.Tk()
+    root.withdraw()
+    root.iconbitmap(find_data_file("icon.ico"))
 
     image = Image.open(find_data_file("icon.ico"))
     menu = (
