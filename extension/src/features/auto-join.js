087dcbae1dbc7a33c0238cdb485871de12342cef
Jason Murray <jsn256@gmail.com>
2025-07-11 12:23:07 +0100

    Detect session transition in auto-join to avoid rejoining the race

--- extension/src/features/auto-join.js
+++ extension/src/features/auto-join.js
@@ -5,6 +5,7 @@ import { findProps } from "../helpers/react-resolver.js";
 import { $ } from "select-dom";
 
 const selector = ".css-qlxuh7 .btn-success";
+const regBarSelector = "#scroll > .css-c980m3";
 
 async function init(activate = true) {
   if (!activate) {
@@ -37,6 +38,26 @@ async function init(activate = true) {
     log(`⛔ Already joined ${lastJoined} once, skipping`);
   }
 }
+
+setInterval(() => {
+  if ($(regBarSelector)) {
+    const regBarProps = findProps($(regBarSelector));
+    let lastJoined = parseInt(localStorage.getItem("iref_last_joined"));
+
+    if (
+      regBarProps.simStatus.status == "Sim Running" &&
+      regBarProps.registrationStatus.subsession_id !== lastJoined
+    ) {
+      log(
+        `🏁 Session transition detected, last session is now ${regBarProps.registrationStatus.subsession_id}`
+      );
+      localStorage.setItem(
+        "iref_last_joined",
+        regBarProps.registrationStatus.subsession_id
+      );
+    }
+  }
+}, 1000);
 
 const id = getFeatureID(import.meta.url);
 const bodyClass = "iref-" + id;
